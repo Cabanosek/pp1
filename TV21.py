222d358910127cd35a385e163df2d8d038126eb3
Cabanosek <56737252+Cabanosek@users.noreply.github.com>
2019-10-29 17:04:23 +0100

    Delete TV21.py

--- TV21.py
+++
@@ -1,7 +0,0 @@
-# Zamiana stopni C na K i F
-
-try:
-    x = int(input("Temperatura w stopniach Celsjusza: "))
-    print(f"{x}°C to {float(x)+273.15}°K i {float(x)*1.8 + 32}°F")
-except ValueError:
-    print("Podaj liczbę.")
